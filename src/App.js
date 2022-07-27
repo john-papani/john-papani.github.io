4d3e7c0ac0c95296e75852507f9376ed779d5ac2
john-papani <johnpapani1@gmail.com>
2022-07-27 17:22:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-// import { Home, Resume, Portfolio, Navbar, Contacts } from './components';
 // import Particles from './components/Particles';
+import About from "./Components/About/About"
 import Home from "./Components/Home/Home";
 import CssBaseline from "@mui/material/CssBaseline";
-import Navbar from "./Components/Navbar/Navbar";
-import Resume from "./Components/Resume/Resume"
+import Splash from "./Components/Splash/Splash";
 const App = () => {
-  return (
-    <>
-      <CssBaseline />
-      <Router>
-        <Routes>
-          
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route path="/resume" element={<Resume />} />
+  const [visited, setVisited] = useState(sessionStorage.getItem("visited"));
+  const update = () => {
+    setVisited(sessionStorage.getItem("visited"));
+  };
 
+  if (visited) {
+    return (
+      <>
+       <CssBaseline />
+    <Router>
+         <Routes>
+           <Route exact path="/" element={<Home />} />
+           <Route exact path="/home" element={<Home />} />
+           <Route path="/about" element={<About />} />
 
-          {/* <Navbar /> */}
-          {/*  
-            <Route path="/resume" component={Resume} />
-            <Route path="/portfolio" component={Portfolio} />
-            <Route path="/contact" component={Contacts} /> */}
-        </Routes>
-      </Router>
+           
+         </Routes>
+       </Router>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <CssBaseline />
+        <Splash update={update} />
+      </>
+    );
+  }
 
-            
+  // return (
+  //   <>
+  //     <CssBaseline />
+  //     <Router>
+  //       <Routes>
+  //         <Route exact path="/" element={<Home />} />
+  //         <Route exact path="/home" element={<Home />} />
+  //         <Route path="/resume" element={<Resume />} />
 
-
-
-    </>
-  );
+  //         {/* <Navbar /> */}
+  //         {/*
+  //           <Route path="/resume" component={Resume} />
+  //           <Route path="/portfolio" component={Portfolio} />
+  //           <Route path="/contact" component={Contacts} /> */}
+  //       </Routes>
+  //     </Router>
+  //   </>
+  // );
 };
 
 export default App;
