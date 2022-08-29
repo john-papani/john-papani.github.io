07412a9a4043058fe9b1b9604b0553aa22d6f852
john-papani <johnpapani1@gmail.com>
2022-08-29 17:00:39 +0300

    Add shortBio text

--- src/Components/ShortBio/ShortBio.js
+++ src/Components/ShortBio/ShortBio.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
 import React from "react";
 import { useStyles } from "./shortBiostyle";
 const ShortBio = () => {
@@ -7,10 +7,31 @@ const ShortBio = () => {
     <>
       <Grid container spacing={2} className={classes.shortbiocontainer}>
         <Grid item xs={12}>
-          <Typography variant="h5">Short Bio</Typography>
+          <Typography variant="h5">About Me </Typography>
         </Grid>
-        <Grid item xs={12}>
-          <div className={classes.biotext}>Sample text for short Bio</div>
+        <Grid
+          container
+          width="85%"
+          alignItems="center"
+          margin="auto"
+          padding="1%"
+          textAlign="justify"
+        >
+          <Grid item xs={12}>
+            <Card>
+              <CardContent>
+                My name is Ioannis Papanikolaou and I am 22 years old.
+                <br />
+                In this period of time I am focusing on my studies, since I am
+                studying at the National Technical University of Athens in the
+                Electrical and Computer Engineering department.
+                <br />I live in Ilioupoli, Athens and I plan to become a full
+                stack developer. I'm looking for a job that suitable with my
+                studied jobscope. I'm open to new ideas always by making them
+                coming in real life with a good team.
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
       </Grid>
     </>
