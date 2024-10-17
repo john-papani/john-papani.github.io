42cfc507e76da320afa05c67542455b0ae345496
john-papani <johnpapani1@gmail.com>
2024-10-17 12:39:35 +0300

    Add volunteering session

---
+++ src/Components/Volunteering/Volunteering.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { volunteering_exper_list } from "./volunteering_exper";
+
+const Volunteering = () => {
+  return (
+    <div id="volunteering" className="pt-[5%] pb-10">
+      <p className="text-2xl text-center font-bold m-6">Volunteering</p>
+      <div className="flex flex-wrap justify-center items-stretch gap-6 w-3/4 mx-auto mb-2">
+        {volunteering_exper_list
+          .sort((a, b) => {
+            // Check if either date has the word "Present"
+            if (a.end_time.includes("Present")) return -1; // "Present" goes to the top
+            if (b.end_time.includes("Present")) return 1;
+            return new Date(b.end_time) - new Date(a.end_time); // Sort by date (descending)
+          })
+          .map((volunteering, index) => (
+            <div
+              key={index}
+              className="flex flex-col w-full md:w-[300px] p-6 bg-black border border-red-700 rounded-lg shadow-lg"
+            >
+              {/* <p>{index}</p> */}
+              <time className="text-xs lg:text-sm text-red-500">
+                {volunteering.start_time} - {volunteering.end_time}
+              </time>
+              <h5 className="text-lg lg:text-xl font-bold tracking-tight text-white">
+                {volunteering.title}
+              </h5>
+              {volunteering.location_url ? (
+                <a
+                  href={volunteering.location_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="italic underline decoration-red-600"
+                >
+                  <h5 className="text-sm lg:text-base font-normal text-red-600">
+                    {volunteering.location}
+                  </h5>
+                </a>
+              ) : (
+                <h5 className="text-sm lg:text-base font-normal text-red-600">
+                  {volunteering.location}
+                </h5>
+              )}
+
+              {/* <ul className="mb-2 text-sm font-normal text-red-300 list-disc list-inside space-y-1"> */}
+              {/* {volunteering.description.split("\n").map((bullet, index) => ( */}
+              {/* <li key={index}>{bullet}</li> */}
+              {/* ))} */}
+              {/* </ul> */}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Volunteering;
---
+++ src/Components/Volunteering/volunteering_exper.js
@@ -0,0 +1,31 @@
+export const volunteering_exper_list = [
+  {
+    start_time: "May 2022",
+    end_time: "April 2022",
+    title: "Volunteer Staff",
+    location:"TEDxNTUA",
+    location_url: "https://tedx-ntua.com/"
+  },
+  {
+    start_time:"February 2024",
+    end_time:"April 2024",
+    title:"Volunteer Staff",
+    location:"IUF Greece",
+    location_url: "https://www.iuf.wolrd/"
+  },
+
+  {
+    start_time:"April 2024",
+    end_time:"Present",
+    title:"Volunteer",
+    location:"Stavros Niarchos Foundation Cultural Center (SNFCC)",
+    location_url: "https://www.snfcc.org/"
+  },
+  {
+    start_time:"February 2024",
+    end_time:"April 2024",
+    title:"Workshop Team",
+    location:"Unique Minds",
+    location_url: "https://uniqueminds.gr/" 
+  }
+];
